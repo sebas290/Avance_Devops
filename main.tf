5a39177579804553f1e3be207c15744615ee3113
sebas290 <Al07004708@tecmilenio.mx>
2025-04-23 07:27:47 -0600

    ditribucion de subredes privadas

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.10.0.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  vpc_id                   = aws_vpc.main.id
+  cidr_block               = "10.10.0.0/24"
+  map_public_ip_on_launch  = true
+  availability_zone        = "us-east-1a"
   tags = { Name = "PublicSubnet" }
 }
 
