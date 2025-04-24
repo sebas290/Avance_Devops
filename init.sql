3ff4056a97d84bd7339b6d080858371effb0b602
sebas290 <Al07004708@tecmilenio.mx>
2025-04-23 22:29:36 -0600

    Creacion de pagina sencilla y script para la tabla de sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+GNU nano 7.2                                                     init.sql
+CREATE DATABASE IF NOT EXISTS Avance;
+USE Avance;
+CREATE TABLE IF NOT EXISTS productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio FLOAT,
+    imagen TEXT,
+    cantidad INT
+);
+
+
